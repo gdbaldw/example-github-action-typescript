e5f2ad8bdf98c8c9e1ed68bdef8be2af8b2a81f1
Doug Baldwin <douglasbaldwin@verizon.net>
2019-11-20 02:47:39 -0500

    debug action

---
+++ .github/actions/debug-action/debug.ts
@@ -0,0 +1,10 @@
+import * as core from '@actions/core'
+
+const run = async (): Promise<void> => {
+    core.debug('👋 Hello! You are an amazing person! 🙌')
+    // Action code here
+}
+
+run()
+
+export default run
